d1e3b0d0341306f64b91a739a7149dd71717a481
ryanfiorini <ryanwfiorini@gmail.com>
2013-01-11 15:31:12 -0500

    Added Comments

--- MEFF_Web_Interfaces/IOperation.cs
+++ MEFF_Web_Interfaces/IOperation.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * This is an ASP.Net adaptaion of the example from Microsofts msdn site.
+ * http://msdn.microsoft.com/en-us/library/dd460648.aspx?cs-save-lang=1&cs-lang=csharp#code-snippet-9
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- MEF_Web_SimpleCalculator/Subtract.cs
+++ MEF_Web_SimpleCalculator/Subtract.cs
@@ -1,4 +1,10 @@
-﻿using MEFF_Web_Interfaces;
+﻿/*
+ * This is an ASP.Net adaptaion of the example from Microsofts msdn site.
+ * http://msdn.microsoft.com/en-us/library/dd460648.aspx?cs-save-lang=1&cs-lang=csharp#code-snippet-9
+ * 
+ */
+
+using MEFF_Web_Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
